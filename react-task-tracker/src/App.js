d59bdad3a658b4d7fc1030b989c0e817a565f3d1
blee-smartcommunications <blee@smartcommunications.com>
2022-08-17 20:45:31 -0400

    Initial build

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+
+  const [tasks, setTask] = useState([
+    {
+      id: 1,
+      text: 'Task 1',
+      day: 'January 5th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Task 2',
+      day: 'Feb 6th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Task 3',
+      day: 'March 16th at 2:30pm',
+      reminder: false
+    }
+  ])
+
+  const name = 'Brian'
+  //const x = false
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title='Hello Task Tracker by:' requiredPropDemo='jessie' />
+      <Tasks tasks={tasks} />
+      <h1>Hello Task Tracker by {name}</h1>
+      {
+
+        /* <h2>Yes or No: {x ? 'Yes' : 'No'}</h2> */
+
+      }
     </div>
   );
 }
---
+++ react-task-tracker/src/components/Header.js
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+
+//const Header = (props) => {
+//<h1>{props.title}</h1>
+
+const onClick = (e) => {
+  console.log(e);
+}
+
+const Header = ({ title }) => {
+  return (
+    <header className='header'>
+      {/* <h1 style={headingStyle}>{title}</h1> */}
+      <h1>{title}</h1>
+      <Button text='Add' onClick={onClick} />
+    </header>
+  )
+}
+
+// this will be overridden if specified
+Header.defaultProps = {
+  title: 'Task Tracker'
+}
+
+Header.propTypes = {
+  title: PropTypes.string,
+  requiredPropDemo: PropTypes.string.isRequired
+}
+
+// CSS in JS
+// const headingStyle = { 
+//   color: 'red', 
+//   backgroundColor : 'blue' 
+// }
+
+export default Header
